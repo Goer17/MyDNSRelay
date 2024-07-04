b6a09109a3a03a64ab80e797473c4b00199b77c2
Linhan Song <146309234+Kitagawayyds@users.noreply.github.com>
2024-07-05 00:53:47 +0800

    Update convert.py

--- Test/convert.py
+++ Test/convert.py
@@ -1,11 +1,10 @@
 import csv
-input_csv_path = 'top.chinaz.com.ip.csv'
 
-_txt_path = 'data.txt'
+input_csv_path = 'top.chinaz.com.ip.csv'
+output_txt_path = 'data.txt'
 with open(input_csv_path, mode='r', newline='', encoding='utf-8') as csv_file:
     csv_reader = csv.reader(csv_file)
     with open(output_txt_path, mode='w', encoding='utf-8') as txt_file:
         for row in csv_reader:
             first_column = row[0]
-            # 写入TXT文件
             txt_file.write(first_column + '\n')
